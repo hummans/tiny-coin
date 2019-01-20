0cf98c0bf116b32d05e7061503477dbf61afbc9a
Shankar <shankqr@gmail.com>
2019-01-20 18:01:41 +0800

    Basic blockchan get requests

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const blockchain = require('./blockchain');
+const tinycoin = new blockchain();
 
 app.listen(3000, () => {
   console.log('Tiny Coin running...');
@@ -7,4 +9,25 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
   res.send('Tiny Coin 1.0');
+});
+
+app.get('/new', (req, res) => {
+  const index = tinycoin.createTransaction(
+    req.query.sender,
+    req.query.recipient,
+    parseInt(req.query.amount)
+  );
+  res.send(`Transaction will be added to Block ${index}`);
+});
+
+app.get('/pool', (req, res) => {
+  res.send(tinycoin.transaction_pool);
+});
+
+app.get('/blocks', (req, res) => {
+  res.send(tinycoin.chain);
+});
+
+app.get('/mine', (req, res) => {
+  res.send(tinycoin.mine('shankar'));
 });
--- blockchain.js
+++ blockchain.js
@@ -5,9 +5,20 @@ class Blockchain {
   constructor() {
     this.chain = [];
     this.transaction_pool = [];
+
+    this.createBlock = this.createBlock.bind(this);
+    this.createTransaction = this.createTransaction.bind(this);
+    this.hash = this.hash.bind(this);
+    this.previousBlock = this.previousBlock.bind(this);
+    this.proofOfWork = this.proofOfWork.bind(this);
+    this.mine = this.mine.bind(this);
+    this.consensus = this.consensus.bind(this);
+    this.discoverPeerChains = this.discoverPeerChains.bind(this);
+
+    this.createBlock(100, 1); //genesis block
   }
 
-  createBlock = (proof, previousHash) => {
+  createBlock(proof, previousHash) {
     const block = {
       index: this.chain.length + 1,
       time_stamp: Date.now(),
@@ -18,18 +29,18 @@ class Blockchain {
     this.transaction_pool = [];
     this.chain.push(block);
     return block;
-  };
+  }
 
-  createTransaction = (sender, receipient, amount) => {
+  createTransaction(sender, receipient, amount) {
     this.transaction_pool.push({
       sender: sender,
       receipient: receipient,
       amount: amount
     });
     return this.previousBlock()['index'] + 1;
-  };
+  }
 
-  hash = block => {
+  hash(block) {
     const blockStr = JSON.stringify(block);
     const hash = crypto
       .createHmac('sha256', 'secret')
@@ -37,35 +48,35 @@ class Blockchain {
       .digest('hex');
 
     return hash;
-  };
+  }
 
-  previousBlock = () => {
+  previousBlock() {
     return this.chain.slice(-1)[0];
-  };
+  }
 
-  proofOfWork = lastProof => {
+  proofOfWork(lastProof) {
     let incrementor = lastProof + 1;
     while (!(incrementor % 9 === 0 && incrementor % lastProof === 0)) {
       incrementor += 1;
     }
     return incrementor;
-  };
+  }
 
-  mine = minerAddress => {
+  mine(minerAddress) {
     const proof = this.proofOfWork(this.previousBlock().proof);
     this.createTransaction('network', minerAddress, 1);
     return this.createBlock(proof, this.hash(this.previousBlock()));
-  };
+  }
 
-  consensus = async () => {
+  async consensus() {
     const otherChains = await this.discoverPeerChains();
     let longestChain = this.chain;
     for (let chain of otherChains)
       if (chain.length > longestChain.length) longestChain = chain;
     this.chain = longestChain;
-  };
+  }
 
-  discoverPeerChains = async () => {
+  async discoverPeerChains() {
     const peerChains = [];
     const peersList = process.env.PEERS.split(',');
     for (let peerUrl of peersList) {
@@ -73,7 +84,7 @@ class Blockchain {
       peerChains.push(peerChain);
     }
     return peerChains;
-  };
+  }
 }
 
 module.exports = Blockchain;
